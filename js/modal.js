b336f9a0a6ff3097a9be31f514a3cf2a91409c3b
Pavel <72277023+PavelDebchuk@users.noreply.github.com>
2020-12-21 20:56:15 +0300

    добавление файлов

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 $(document).ready(function(){
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
 });
+=======
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+>>>>>>> добавление файлов
