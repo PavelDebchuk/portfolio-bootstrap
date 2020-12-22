c42741c99dd29ebb32e6b0dc6b961391a02afbd0
AlexYanchuk <alex7devel@gmail.com>
2020-12-23 00:07:53 +0200

    add query

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 $(document).ready(function(){
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
 });
-=======
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
->>>>>>> добавление файлов
